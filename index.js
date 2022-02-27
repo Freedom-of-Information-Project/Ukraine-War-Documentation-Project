1b49b8cb5abeffc01895457c83db1b2de8a81684
TudbuT <tudbut@tudbut.de>
2022-02-27 15:47:25 +0100

    Customized forum to fit needs, added post scraper, implemented reddit api

---
+++ index.js
@@ -0,0 +1,155 @@
+const Express = require('express')
+const bdb = require('bdb.js')
+const PostGetter = require('./postgetter.js')
+const postGetter = new PostGetter({
+    reddit: [
+        "r/ww3",
+        "r/CombatFootage",
+        "r/russia",
+        "r/ukraine"
+    ],
+    twitter: [
+        "WW3updated"
+    ],
+    save: bdb.load('postgetter.json', 1)
+})
+
+const posts = bdb.load('posts.json', 1)
+
+const webname = 'The Freedom Of Press Project'
+const email = 'fopp@mail.tudbut.de'
+
+const server = new Express()
+
+server.set('view engine', 'ejs')
+
+function replace(req, regex, repl) {
+    const url = req.url.replaceAll(regex, repl)
+    console.log(url)
+    const parsedUrl = new URL('http://localhost' + url)
+    req.url = url
+    req.originalUrl = url
+    req.path = parsedUrl.pathname
+    req.search = parsedUrl.search
+    req._parsedUrl = parsedUrl
+    const query = {}
+    for(const entry of parsedUrl.searchParams) {
+        query[entry[0]] = entry[1]
+    }
+    req.query = query
+}
+
+postGetter.onPost(function onPost(post) {
+    let dbpost = { timestamp: new Date().getTime(), author: post.source + '/' + post.authorName, title: post.title, content: post.text, comments: [ { author: 'SYSTEM', title: 'Post source', content: post.url, comments: [] } ] }
+
+    // Append images
+    for(let image of post.images) {
+        content += '\n' + `[# ${image} #]`
+    }
+
+    posts.push(dbpost)
+})
+postGetter.getReddit()
+postGetter.getTwitter()
+
+server.use(function replacer(req, res, next) {
+    replace(req, /^\/post\/([0-9]+)_?/g, '/post?id=$1')
+    replace(req, /^\/comment\/([0-9_]+)\/([0-9_]*)(\?(.*))?/g, '/comment?id=$1&comment=$2&$4')
+    next()
+})
+
+server.use(require('body-parser').urlencoded({extended: false}))
+
+
+server.all('/', function get(req, res) {
+    const fake = req.body.fake === 'yes'
+    let mainPage = {author: webname, title: 'All posts', content: 'These are all the posts on the board:', comments: []}
+    for (let i = 0; i < posts.length && i < 2000; i++) {
+        mainPage.comments.push({timestamp: posts[i].timestamp, author: posts[i].author, title: posts[i].title, content: posts[i].content, comments: []}) 
+    }
+    if(req.body.name && req.body.title && req.body.content) {
+        let post = {timestamp: new Date().getTime(), author: req.body.name, title: req.body.title, content: req.body.content.replaceAll('\r\n', '\n'), comments: []}
+        if(fake) {
+            mainPage.comments.push(post)
+            res.render('post.ejs', {post: mainPage, postid: '-1', webname: webname, email: email, comment: '', fake: true})
+        } else {
+            posts.push(post)
+            res.redirect(`/`)
+        }
+        return
+    }
+    res.render('post.ejs', {post: mainPage, postid: '-1', webname: webname, email: email, comment: '', fake: false})
+})
+server.get('/post', function get(req, res) {
+    if(req.query.id) {
+        let id = req.query.id
+        if(posts[id]) {
+            res.render('post.ejs', {post: posts[id], postid: id, webname: webname, email: email, comment: null, fake: false})
+        }
+    }
+})
+server.all('/comment', function get(req, res) {
+    if(req.query.id) {
+        let id = req.query.id
+        let comment = req.query.comment 
+        if(posts[id]) {
+            const fake = req.body.fake === 'yes'
+            let toRemove = null
+            let cid = ''
+            try {
+                function recurse(post) {
+                    console.log(String(cid) + ' ' + comment)
+                    if(String(cid) === comment) {
+                        if(req.body.name && req.body.title && req.body.content) { 
+                            post.comments.push({timestamp: new Date().getTime(), author: req.body.name, title: req.body.title, content: req.body.content.replaceAll('\r\n', '\n'), comments: []})
+                            if(fake) {
+                                res.render('post.ejs', {post: posts[id], postid: id, webname: webname, email: email, comment: cid, fake: fake})
+                                toRemove = post
+                            } else
+                                res.redirect(`/post/${id}`)
+                            cid = -1
+                        }
+                        return true
+                    }
+                    for(let i = 0; i < post.comments.length; i++) {
+                        const pid = cid
+                        cid += i + '_'
+                        if(recurse(post.comments[i]))
+                            return true
+                        cid = pid
+                    }
+                    return false
+                }
+                const f = recurse(posts[id]);
+                if(f && cid == -1)
+                    return
+                if(f) {
+                    res.render('post.ejs', {post: posts[id], postid: id, webname: webname, email: email, comment: cid, fake: fake})
+                }
+                else {
+                    if(req.body.name && req.body.title && req.body.content) { 
+                        posts[id].comments.push({timestamp: new Date().getTime(), author: req.body.name, title: req.body.title, content: req.body.content.replaceAll('\r\n', '\n'), comments: []})
+                        if(fake) {
+                            res.render('post.ejs', {post: posts[id], postid: id, webname: webname, email: email, comment: cid, fake: fake})
+                            toRemove = posts[id]
+                        } else
+                            res.redirect(`/post/${id}`)
+                    }
+                    else
+                        res.render('post.ejs', {post: posts[id], postid: id, webname: webname, email: email, comment: ''})
+                }
+            } finally {
+                if(fake) {
+                    toRemove.comments.pop()
+                }
+            }
+        }
+    }
+    else
+        res.send("err1")
+})
+
+let PORT = process.argv[2]
+if(!PORT) PORT = process.env.PORT
+if(!PORT) PORT = 8080
+server.listen(Number(PORT), () => console.log(PORT))
---
+++ postgetter.js
@@ -0,0 +1,74 @@
+const http = require('follow-redirects').https
+let lastIDs = { 
+    reddit: {}, twitter: {}
+}
+
+function getRedditPosts(notify, subreddit) {
+    // TODO This is callback hell
+    http.get(`https://reddit.com/${subreddit}/new.json`, res => {
+        const { statusCode } = res;
+        let error;
+        // Any 2xx status code signals a successful response but
+        // here we're only checking for 200.
+        if (statusCode !== 200) {
+          error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        }
+        if (error) {
+          console.error(error.message);
+          // Consume response data to free up memory
+          res.resume();
+          return;
+        }
+        res.setEncoding('utf8')
+        let data = ''
+        res.on('data', d => data += d)
+        res.on('end', function redditDataGot() {
+            try {
+                const parsed = JSON.parse(data)
+                let newPosts = []
+                for(let post of parsed.data.children) {
+                    post = post.data
+                    if(post.id === lastIDs.reddit[subreddit]) {
+                        break // Exit loop, the posts following this are already indexed
+                    }
+                    console.log("Found new post!")
+                    newPosts.unshift(post)
+                }
+                for(let post of newPosts) {
+                    if(post.selftext === '')
+                        continue
+                    let unified = { source: 'reddit:' + subreddit.substring(2), authorName: post.author, title: post.title, text: post.selftext.replace(/\[.*?\]\((.*?)\)/g, '$1'), url: post.url, images: [] }
+                    notify(unified)
+                    lastIDs.reddit[subreddit] = post.id
+                }
+            } catch(e) {
+                console.log(e)
+                console.log(data)
+            }
+        })
+    }).on('error', console.log)
+}
+
+module.exports = function (config) {
+    lastIDs = config.save.lastIDs || lastIDs
+    let notifiers = []
+    function getReddit() {
+        for(let subreddit of config.reddit) {
+            getRedditPosts( function notify(data) { for(let notifier of notifiers) notifier(data) }, subreddit)
+        }
+        config.save.lastIDs = lastIDs
+    }
+    setInterval(getReddit, 30000)
+    function getTwitter() {
+        // TODO implement this
+    }
+    setInterval(getTwitter, 60000)
+    return {
+        getReddit: getReddit,
+        getTwitter: getTwitter,
+        onPost: function onPost(notifier) {
+            notifiers.push(notifier)
+        }
+    }
+}
